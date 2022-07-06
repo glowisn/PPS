42186dac1ed36630849ad9d712144354b2789256
Yechan Jung <asdxcv1998@gmail.com>
2022-07-06 17:22:23 +0900

    12

---
+++ week1/A012_정예찬_21700663.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        bool *isPrime = new bool[n];
+
+        int count = 0;
+        for (int i = 2; i < n; i++)
+        {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++)
+        {
+            if (!isPrime[i])
+                continue;
+            for (int j = i * i; j < n; j += i)
+            {
+                isPrime[j] = false;
+            }
+        }
+        for (int i = 2; i < n; i++)
+        {
+            if (isPrime[i])
+                count++;
+        }
+        return count;
+    }
+};
