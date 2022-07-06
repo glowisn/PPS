e64ad9b2b4c472105e4dcb3cc3a3b3d23cd73112
Yechan Jung <asdxcv1998@gmail.com>
2022-07-06 11:20:42 +0900

    5

---
+++ week1/A005_정예찬_21700663.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    map<char, int> map;
+    for(int i = 0;i < skill.length();i++){
+        map[skill[i]] = i + 1;
+    }
+    for(auto skill_tree : skill_trees)
+    {
+        int check = 1;
+        bool isOK = true;
+        for(int i = 0; i < skill_tree.length();i++){
+            if(map[skill_tree[i]] == check){
+                check++;
+                continue;
+            }
+            else if(map[skill_tree[i]] > check){
+                isOK = false;
+                break;
+            }
+        }
+        if(isOK) answer++;
+    }
+
+    return answer;
+}
