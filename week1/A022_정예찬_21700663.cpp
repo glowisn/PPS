bda6930d480f5ec18186ebb3ac8fbd63730b3a9a
Yechan Jung <asdxcv1998@gmail.com>
2022-07-08 15:52:44 +0900

    22

---
+++ week1/A022_정예찬_21700663.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int *time = new int[N];
+
+    int Y = 0, G = 0;
+
+    for(int i = 0 ;i < N; i++)
+    {
+        cin >> time[i];
+        Y += (time[i] / 30 + 1) * 10;
+        G += (time[i] / 60 + 1) * 15;
+    }
+    if(Y < G)
+    {
+        cout << "Y " << Y << endl; 
+    }
+    else if(Y > G)
+    {
+        cout << "M " << G << endl;
+    }
+    else
+    {
+        cout << "Y M " << Y << endl;
+    }
+    return 0;
+}
