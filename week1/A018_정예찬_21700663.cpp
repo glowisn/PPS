164331ba1ed742e7d5a9a2e1da07f1b84bf6cc02
Yechan Jung <asdxcv1998@gmail.com>
2022-07-08 15:59:06 +0900

    18

---
+++ week1/A018_정예찬_21700663.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int *a = new int[N];
+    int *b = new int[N];
+    for(int i = 0; i < N; i++)
+    {
+        cin >> a[i];
+    } for(int i = 0; i < N; i++)
+    {
+        cin >> b[i];
+    }
+    sort(a,a + N);
+    sort(b,b + N, greater<int>());
+    int sum = 0;
+    for(int i = 0; i < N ;i++){
+        sum += a[i] * b[i];
+    }
+    cout << sum << endl;
+    return 0;
+}
