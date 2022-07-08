44665f7913e64b08dbd5b1a5c32972d95f5bafd8
Yechan Jung <asdxcv1998@gmail.com>
2022-07-08 13:40:02 +0900

    21

---
+++ week1/A021_정예찬_21700663.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int *plugs = new int[N];
+    int maxp =0;
+    for(int i = 0; i < N;i++){
+        cin >> plugs[i];
+        maxp += (plugs[i] - 1);
+    }
+    cout << maxp + 1 << endl;
+    
+    return 0;
+}
