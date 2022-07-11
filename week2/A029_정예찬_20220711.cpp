1f2b97eb6ccd4c1a20babfd8acff3dcc34d27f26
Yechan Jung <asdxcv1998@gmail.com>
2022-07-11 14:47:07 +0900

    29

--- week2/A029_정예찬_20220711.cpp
+++ week2/A029_정예찬_20220711.cpp
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/17210
+
 #include <iostream>
 using namespace std;
 
@@ -6,10 +8,15 @@ int main()
     unsigned int N;
     int wayF;
     cin >> N >> wayF;
-    int *w = new int[N];
 
-    for(int i = 0; i < N ;i++){
-        
+    if(N > 5){
+        cout << "Love is open door" << endl;
+        return 0;
+    }
+    for(int i = 0; i < N -1 ; i++)
+    {
+        wayF = !wayF;
+        cout << wayF << endl;
     }
 
     return 0;
