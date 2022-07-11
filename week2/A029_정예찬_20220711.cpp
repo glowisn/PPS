e6c4ea32579a6e6e9308210ca712900bc1e57efe
Yechan Jung <asdxcv1998@gmail.com>
2022-07-11 14:30:44 +0900

    pps

---
+++ week2/A029_정예찬_20220711.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int N;
+    int wayF;
+    cin >> N >> wayF;
+    int *w = new int[N];
+
+    for(int i = 0; i < N ;i++){
+        
+    }
+
+    return 0;
+}
